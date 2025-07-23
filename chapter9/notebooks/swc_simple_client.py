99eb1d247936bfddcfa1019f785154d21add7fee
Nagarjuna Gottipati <64250120+NagarjunaD024@users.noreply.github.com>
2025-07-23 05:10:20 +0000

    Adding error functionality

--- chapter9/notebooks/swc_simple_client.py
+++ chapter9/notebooks/swc_simple_client.py
@@ -18,3 +18,27 @@ logger = logging.getLogger(__name__)
     max_time=5,  
     jitter=backoff.random_jitter  
 )
+
+def call_api_endpoint(
+    base_url: str,
+    api_endpoint: str, 
+    api_params: dict = None
+) -> httpx.Response:
+
+    try:
+        with httpx.Client(base_url=base_url) as client: 
+            logger.debug(f"base_url: {base_url}, api_endpoint: {api_endpoint}")
+            response = client.get(api_endpoint, params=api_params)
+            response.raise_for_status()
+            logger.debug(f"Response JSON: {response.json()}")
+            return response
+    except httpx.HTTPStatusError as e:
+        logger.error(f"HTTP status error occurred: {e.response.text}")
+        return httpx.Response(status_code=e.response.status_code, 
+                              content=b"API error")
+    except httpx.RequestError as e:
+        logger.error(f"Request error occurred: {str(e)}")
+        return httpx.Response(status_code=500, content=b"Network error")
+    except Exception as e:
+        logger.error(f"Unexpected error occurred: {str(e)}")
+        return httpx.Response(status_code=500, content=b"Unexpected error")
